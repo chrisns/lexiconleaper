9a8eac5c9d1b751182dfd1bd7ac0318684526744
Chris Nesbitt-Smith <chris@cns.me.uk>
2023-12-19 09:20:18 +0000

    linted

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -13,7 +13,7 @@ var letters = Array.from({ length: 26 }, (_, i) => String.fromCharCode(i + 97))
 
 const playLetter = computed(() => letters[letterIndex.value])
 const hints = computed(() =>
-// @ts-ignore: Unreachable code error
+  // @ts-ignore: Unreachable code error
   games[game.value].filter((item) => item.charAt(0).toLowerCase() == playLetter.value)
 )
 const showHints = ref(false)
@@ -42,7 +42,7 @@ function play() {
 
   if (randomLetter.value == true)
     letterIndex.value = Math.floor(Math.random() * viableLetters.length)
-  else if (letterIndex.value == viableLetters.length-1) letterIndex.value = 0
+  else if (letterIndex.value == viableLetters.length - 1) letterIndex.value = 0
   else letterIndex.value++
 
   countdown.value = timeAllowance
@@ -68,7 +68,9 @@ randomizeGame()
     </button>
     <button @click="reload">Restart</button>
     <button @click="showHints = !showHints" :class="{ on: showHints }">Hints</button>
-    <button @click="showInstructions = !showInstructions" :class="{ on: showInstructions }">How to play</button>
+    <button @click="showInstructions = !showInstructions" :class="{ on: showInstructions }">
+      How to play
+    </button>
   </div>
   <ul id="hints" v-if="showHints">
     <li v-for="hint in hints" :key="hint">
@@ -78,12 +80,19 @@ randomizeGame()
   <div id="instructions" v-if="showInstructions">
     <h2>How to play</h2>
     <p>You will be presented with a category and a letter.</p>
-    <p>You or someone in your group must name something that matches the category that begins with the letter before the time runs out</p>
+    <p>
+      You or someone in your group must name something that matches the category that begins with
+      the letter before the time runs out
+    </p>
     <p>You will be presented a random category when the game starts.</p>
     <p>You will not get a letter if there isn't a valid answer</p>
-    <p>If you want to mix things up you can select to have a <a @click="randomGame = !randomGame">random category</a> and/or <a @click="randomLetter = !randomLetter">random letter</a> on each play</p>
+    <p>
+      If you want to mix things up you can select to have a
+      <a @click="randomGame = !randomGame">random category</a> and/or
+      <a @click="randomLetter = !randomLetter">random letter</a> on each play
+    </p>
     <p>If you run out of ideas, you can <a @click="showHints = !showHints">show the hints</a></p>
-</div>
+  </div>
 </template>
 
 <style scoped>
@@ -92,7 +101,9 @@ button.on {
   color: white;
 }
 button {
-  transition: background-color 0.5s ease, color 0.5s ease;
+  transition:
+    background-color 0.5s ease,
+    color 0.5s ease;
   background-color: rgb(175, 175, 175);
   font-size: 2em;
   margin: 0.2em;
