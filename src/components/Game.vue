1ac722cc5319c2459b835471522de79accf0f314
Chris Nesbitt-Smith <chris@cns.me.uk>
2023-12-19 00:46:29 +0000

    start on A

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -11,8 +11,10 @@ var letterIndex = ref(0)
 var letters = Array.from({ length: 26 }, (_, i) => String.fromCharCode(i + 97))
 
 const playLetter = computed(() => letters[letterIndex.value])
-const hints = ref([])
-const showHints = ref(true)
+// @ts-ignore: Unreachable code error
+const hints = computed(() => games[game.value].filter((item) => item.charAt(0).toLowerCase() == playLetter.value))
+// const hints = ref([])
+const showHints = ref(false)
 const timeAllowance = 10
 const countdown = ref(timeAllowance)
 
@@ -41,14 +43,11 @@ function play() {
   else if (letterIndex.value >= viableLetters.length) letterIndex.value = 0
   else letterIndex.value++
 
-  // @ts-ignore: Unreachable code error
-  hints.value = games[game.value].filter((item) => item.charAt(0).toLowerCase() == playLetter.value)
   countdown.value = timeAllowance
   showHints.value = false
 }
 
 randomizeGame()
-play()
 </script>
 
 <template>
