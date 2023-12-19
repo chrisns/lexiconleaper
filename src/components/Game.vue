d78b244add676ba397999909b2366220f18b60a0
Chris Nesbitt-Smith <chris@cns.me.uk>
2023-12-19 09:05:09 +0000

    add how to instructions

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -6,6 +6,7 @@ import games from '../assets/games.json'
 
 const randomGame = ref(false)
 const randomLetter = ref(false)
+const showInstructions = ref(false)
 var game = ref(Object.keys(games)[0])
 var letterIndex = ref(0)
 var letters = Array.from({ length: 26 }, (_, i) => String.fromCharCode(i + 97))
@@ -47,6 +48,9 @@ function play() {
   countdown.value = timeAllowance
   showHints.value = false
 }
+function reload() {
+  location.reload()
+}
 
 randomizeGame()
 </script>
@@ -62,21 +66,33 @@ randomizeGame()
     <button @click="randomLetter = !randomLetter" :class="{ on: randomLetter }">
       Random Letter
     </button>
+    <button @click="reload">Restart</button>
     <button @click="showHints = !showHints" :class="{ on: showHints }">Hints</button>
+    <button @click="showInstructions = !showInstructions" :class="{ on: showInstructions }">How to play</button>
   </div>
   <ul id="hints" v-if="showHints">
     <li v-for="hint in hints" :key="hint">
       {{ hint }}
     </li>
   </ul>
+  <div id="instructions" v-if="showInstructions">
+    <h2>How to play</h2>
+    <p>You will be presented with a category and a letter.</p>
+    <p>You or someone in your group must name something that matches the category that begins with the letter before the time runs out</p>
+    <p>You will be presented a random category when the game starts.</p>
+    <p>You will not get a letter if there isn't a valid answer</p>
+    <p>If you want to mix things up you can select to have a <a @click="randomGame = !randomGame">random category</a> and/or <a @click="randomLetter = !randomLetter">random letter</a> on each play</p>
+    <p>If you run out of ideas, you can <a @click="showHints = !showHints">show the hints</a></p>
+</div>
 </template>
 
 <style scoped>
 button.on {
   background-color: green;
+  color: white;
 }
 button {
-  transition: background-color 0.5s ease;
+  transition: background-color 0.5s ease, color 0.5s ease;
   background-color: rgb(175, 175, 175);
   font-size: 2em;
   margin: 0.2em;
@@ -84,6 +100,15 @@ button {
 }
 #category {
   font-size: 4rem;
+}
+#instructions {
+  text-align: center;
+  max-width: 500px;
+  margin: 0 auto;
+  color: white;
+}
+#instructions a {
+  text-decoration: underline;
 }
 #letter {
   font-size: 20rem;
